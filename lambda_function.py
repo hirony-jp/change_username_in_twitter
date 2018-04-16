b851774e6bfd38e1fbacd5b2ebfb74e210f661e7
hirony-jp <jp.hiroki48@gmail.com>
2018-04-17 00:57:12 +0900

    夜の晴れを太陽から月へ変更

--- lambda_function.py
+++ lambda_function.py
@@ -1,4 +1,5 @@
 import json
+import datetime as dt
 import requests
 from requests_oauthlib import OAuth1Session
 
@@ -19,6 +20,8 @@ def lambda_handler(event, context):
     weather_url = "http://api.openweathermap.org/data/2.5/weather?q={city}&APPID={key}"
     weather_url = weather_url.format(city = city_name, key = weather_key)
     twitter_url = "https://api.twitter.com/1.1/account/update_profile.json"
+    now = dt.datetime.now()
+    jst = now + dt.timedelta(hours=9) 
 
     # 天気データを取得
     weather_req = requests.get(weather_url)
@@ -28,8 +31,12 @@ def lambda_handler(event, context):
     # weather id をもとに分類
     if weather_id == 800:
         username = "ひろにぃ☀快晴"
+        if jst >= 18 and jst <= 23 or jst >= 0 and jst <= 5:
+            username = "ひろにぃ🌕快晴"
     elif weather_id >= 801:
-        username = "ひろにぃ⛅晴れ"
+        username = "ひろにぃ☀☁晴れ"
+        if jst >= 18 and jst <= 23 or jst >= 0 and jst <= 5:
+            username = "ひろにぃ🌕☁晴れ"
     elif weather_id >= 802 and weather_id <= 804:
         username = "ひろにぃ☁くもり"
     elif weather_id >= 300 and weather_id <= 321:
@@ -43,6 +50,7 @@ def lambda_handler(event, context):
     elif weather_id >= 900:
         username = "ひろにぃ🌀やっべぇ"
 
+    # set username parameter.
     params = { "name" : username }
 
     # update Twitter username.
