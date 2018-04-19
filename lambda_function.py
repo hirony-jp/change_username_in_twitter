8182e557d436453765b005e88c9eefb51f873a60
hirony-jp <jp.hiroki48@gmail.com>
2018-04-19 22:50:28 +0900

    update

--- lambda_function.py
+++ lambda_function.py
@@ -20,10 +20,11 @@ def lambda_handler(event, context):
     weather_url = "http://api.openweathermap.org/data/2.5/weather?q={city}&APPID={key}"
     weather_url = weather_url.format(city = city_name, key = weather_key)
     twitter_url = "https://api.twitter.com/1.1/account/update_profile.json"
+    # AWS Lambda は UST なので JST に変えてやる必要がある
     now = dt.datetime.now()
-    # AWS Lambda は UST なので JST に変換してやる必要がある。
     jst = now + dt.timedelta(hours=9)
     jst = jst.hour
+    username = "ひろにぃ"
 
     # 天気データを取得
     weather_req = requests.get(weather_url)
@@ -32,25 +33,27 @@ def lambda_handler(event, context):
 
     # weather id をもとに分類
     if weather_id == 800:
-        username = "ひろにぃ☀快晴"
         if jst >= 18 and jst <= 23 or jst >= 0 and jst <= 5:
-            username = "ひろにぃ🌕快晴"
+            username = username + "🌕快晴"
+        else:
+            username = username + "☀快晴"
     elif weather_id >= 801:
-        username = "ひろにぃ☀☁晴れ"
         if jst >= 18 and jst <= 23 or jst >= 0 and jst <= 5:
-            username = "ひろにぃ🌕☁晴れ"
+            username = username + "🌕☁晴れ"
+        else:
+            username = username + "☀☁晴れ"
     elif weather_id >= 802 and weather_id <= 804:
-        username = "ひろにぃ☁くもり"
+        username = username + "☁くもり"
     elif weather_id >= 300 and weather_id <= 321:
-        username = "ひろにぃ🌂霧雨"
+        username = username + "🌂霧雨"
     elif weather_id >= 500 and weather_id <= 531:
-        username = "ひろにぃ☔あめ"
+        username = username + "☔あめ"
     elif weather_id >= 200 and weather_id <= 232:
-        username = "ひろにぃ⚡☔雷雨"
+        username = username + "⚡☔雷雨"
     elif weather_id >= 600 and weather_id <= 622:
-        username = "ひろにぃ⛄ゆき"
+        username = username + "⛄ゆき"
     elif weather_id >= 900:
-        username = "ひろにぃ🌀やっべぇ"
+        username = username + "🌀やっべぇ"
 
     # set username parameter.
     params = { "name" : username }
